87a84b738db33c3a1dc0c005edcb5626468fa167
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2026-06-02 15:14:32 +0530

    Update faq.py

--- app/resources/faq.py
+++ app/resources/faq.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from pathlib import Path
 import chromadb
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+
 
 
 
@@ -8,6 +14,7 @@ faqs_path = Path(__file__).parent/"/resources/faq_data.csv"
 chroma_client = chromadb.Client()
 collection_name_faq='faqs'
 
+Groq_client = Groq()
 
 
 def ingest_faq_data(path):
@@ -30,7 +37,13 @@ def ingest_faq_data(path):
         print(f"Collection {collection_name_faq} already exists")
     
 
-
+def get_relevant_qa(query):
+    collection = chroma_client.get_collection(name=collection_name_faq)
+    result = collection.query(
+        query_texts=[query],
+        n_results=2
+    )
+    return result
 
 if __name__ == "__main__":
     ingest_faq_data(faqs_path)

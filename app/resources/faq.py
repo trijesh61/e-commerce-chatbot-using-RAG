a9428d7f344e58803a986201c11840a11110baaa
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2026-06-02 16:03:59 +0530

    Update faq.py

--- app/resources/faq.py
+++ app/resources/faq.py
@@ -1,9 +1,11 @@
+from unittest import result
+
 import pandas as pd
 from pathlib import Path
 import chromadb
 from dotenv import load_dotenv
 from groq import Groq
-
+import os
 load_dotenv()
 
 
@@ -14,7 +16,7 @@ faqs_path = Path(__file__).parent/"/resources/faq_data.csv"
 chroma_client = chromadb.Client()
 collection_name_faq='faqs'
 
-Groq_client = Groq()
+groq_client = Groq()
 
 
 def ingest_faq_data(path):
@@ -45,5 +47,45 @@ def get_relevant_qa(query):
     )
     return result
 
+
+def faq_chain(query):
+    result = get_relevant_qa(query)
+
+    context = ''.join(
+        [r.get('answer', '') for r in result['metadatas'][0]]
+    )
+
+    answer = generate_answer(query, context)
+
+    return answer
+
+def generate_answer(query, context):
+    prompt = f"""
+Given the question and context below, generate the answer based on the context only.
+If you don't find the answer inside the context then say "I don't know".
+Do not make things up.
+
+QUESTION: {query}
+
+CONTEXT: {context}
+"""
+
+    chat_completion = groq_client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+               model=os.environ["GROQ_MODEL"],
+    )
+
+    return chat_completion.choices[0].message.content
+
+
 if __name__ == "__main__":
     ingest_faq_data(faqs_path)
+    query="what is your policy on defective Products?"
+    answer=faq_chain(query)
+
+
